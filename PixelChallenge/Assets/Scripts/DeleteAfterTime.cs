bc81619126d74aabfce85517a5a84bc29914531c
Sharundaar <rabiller.donatien@hotmail.fr>
2017-04-05 23:27:08 -0400

    Initial Commit. Updated .gitignore to match actual files hierarchy.

---
+++ PixelChallenge/Assets/Scripts/DeleteAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteAfterTime : MonoBehaviour {
+
+	public float TimeLimit;
+
+	private float tAcc = 0;
+
+	// Update is called once per frame
+	void Update () {
+		tAcc += Time.deltaTime;
+		if(tAcc >= TimeLimit)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ PixelChallenge/Assets/Scripts/PrefabThrower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabThrower : MonoBehaviour {
+
+	public Rigidbody Prefab;
+	public float ForceStrength;
+	public float Frequency;
+
+	private float t;
+	
+	int counter = 0;
+
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+		while( t > 1.0f / Frequency )
+		{
+			t -= 1.0f / Frequency;
+			var go = Instantiate( Prefab, transform.position, Quaternion.identity);
+			go.AddForce( transform.forward * ForceStrength, ForceMode.Impulse );
+			go.name = Prefab.name + counter++;
+		}
+	}
+}
