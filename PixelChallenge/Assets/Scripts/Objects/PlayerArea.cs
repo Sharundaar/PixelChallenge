3ba7e11ce0450b1c79fa279a20a1385e308cbfc9
Benjamin <bendescamps@wanadoo.fr>
2017-04-08 03:37:24 +0200

    Moving scripts in new folders

---
+++ PixelChallenge/Assets/Scripts/Objects/PlayerArea.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArea : MonoBehaviour {
+
+	public int playerId;
+	public int currentScore;
+
+	private List<CollectibleObject> objects = new List<CollectibleObject>();
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		var collectible = collider.GetComponent<CollectibleObject> ();
+		if (collectible) {
+			Debug.Log ("ENTER");
+			collectible.SetIsInPlayerArea (true);
+			objects.Add (collectible);
+			currentScore += collectible.points;
+			return;
+		} 
+
+		var playerData = collider.GetComponentInParent<PlayerData> ();
+		if (playerData && playerData.PlayerId != playerId) {
+			Vector3 forceDirection = (collider.transform.position - transform.position);
+			forceDirection.Normalize ();
+			ImpulseForce force = playerData.gameObject.AddComponent<ImpulseForce> ();
+			force.SetForceAndDuration (forceDirection * 10, 0.5f);
+		}
+		
+	}
+
+	void OnTriggerStay(Collider collider)
+	{
+
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		var collectible = collider.GetComponent<CollectibleObject> ();
+		if (collectible) {
+			Debug.Log ("QUIT");
+			collectible.SetIsInPlayerArea (false);
+			objects.Remove (collectible);
+			currentScore -= collectible.points;
+		}
+	}
+
+	public int RecalculatePoints()
+	{
+		currentScore = 0;
+		foreach (CollectibleObject collectible in objects) {
+			currentScore += collectible.points;
+		}
+		return currentScore;
+	}
+
+	public List<CollectibleObject> GetObjects()
+	{
+		return objects;
+	}
+
+
+
+}
---
+++ PixelChallenge/Assets/Scripts/Objects/Sun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//transform.RotateAround (Vector3.zero, new Vector3(0.3f, 0, 1), 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.RotateAround (Vector3.zero, new Vector3(0.3f, 0, 1), 10f * Time.deltaTime);
+		transform.RotateAround (Vector3.zero, new Vector3(0.3f, 0, 1), 10f * Time.deltaTime);
+
+		//Vector3 objRotation = transform.rotation.eulerAngles;
+		//Debug.Log (objRotation);
+		//float clampedY = Mathf.Clamp (objRotation.y, -30f, 30f);
+		//transform.rotation.eulerAngles = new Vector3 (transform.position.x + 1, transform.position.y, transform.position.z);
+	}
+}
